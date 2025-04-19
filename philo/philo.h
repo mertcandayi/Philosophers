3347cc3e16c8961e882979aa5f7392078909a2b5
medayi <mrtcn15.dy@gmail.com>
2025-04-19 22:51:34 +0300

    baby step

--- philo/philo.h
+++ philo/philo.h
@@ -1,7 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: medayi    <medayi@student.42istanbul>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/19 15:38:36 by medayi            #+#    #+#             */
+/*   Updated: 2025/04/19 22:49:35 by medayi           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef PHILO_H
 # define PHILO_H
 
 # include <stdio.h>
+# include <stdlib.h>
+# include <string.h>
+# include <unistd.h>
+# include <pthread.h>
+# include <sys/time.h>
 
 # define RESET "\033[0m"
 # define RED "\033[1;31m"
@@ -11,5 +28,12 @@
 # define MAGENTA "\033[1;35m"
 # define CYAN "\033[1;36m"
 # define WHITE "\033[1;37m"
+
+# define MAX_PHILOSOPHERS 200
+# define MIN_PHILOSOPHERS 1
+
+typedef struct s_philo
+{
+}	t_philo;
 
 #endif

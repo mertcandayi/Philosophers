d309f31c502471c6204179a2adbf6d7033d0ad77
medayi <mrtcn15.dy@gmail.com>
2025-04-19 19:50:46 +0300

    Dining Philosophers

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# 🍜Philosophers
-# 🤵🏻‍♂️Filozofların Akşam Yemeği (Dining Philosophers)
+# 🍜 Philosophers
+
+# 🤵🏻‍♂️ Filozofların Akşam Yemeği (Dining Philosophers)
+
+## 🧠 Problemin Kökeni
+
+Dining Philosophers problemi, 1965 yılında bilgisayar bilimci Edsger Dijkstra tarafından ortaya konmuş, daha sonra Tony Hoare tarafından şimdiki formuyla düzenlenmiş klasik bir senkronizasyon problemidir. Problem, bilgisayar biliminde eşzamanlılık (concurrency) kavramını ve paylaşılan kaynakların senkronizasyonunu öğretmek için kullanılır.
--- philo/main.c
+++ philo/main.c
@@ -4,8 +4,8 @@ void	ft_usage(char *program_name)
 {
 	printf("\n           %sPHILOSOPHERS USAGE%s           \n\n", CYAN, RESET);
 	printf("%sError:%s Invalid arguments\n", RED, RESET);
-	printf("%sUsage:%s %s %s<philos> <time_die> <time_eat> <time_sleep> [meals]%s\n\n", 
-		GREEN, RESET, program_name, BLUE, RESET);
+	printf("%sUsage:%s %s %s<philos> <time_die>%s", GREEN, RESET, program_name, BLUE, RESET);
+	printf(" %s<time_eat> <time_sleep> [meals]%s\n\n", BLUE, RESET);
 	printf("%sParameters:%s\n", MAGENTA, RESET);
 	printf("  %s<philos>%s     : Number of philosophers and forks\n", BLUE, RESET);
 	printf("  %s<time_die>%s   : Time in ms until a philosopher dies without eating\n", BLUE, RESET);
@@ -16,7 +16,7 @@ void	ft_usage(char *program_name)
 
 int main(int ac, char **av)
 {
-    if(ac != 4 || ac != 5)
+    if (ac != 4 || ac != 5)
     {
         ft_usage(av[0]);
         return (0);

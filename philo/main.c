6cbfd51fa12df9af0476231ae3f9c9bc64cd4a29
medayi <mrtcn15.dy@gmail.com>
2025-04-20 08:59:09 +0300

    düşünüyorum öyleyse varım

--- philo/main.c
+++ philo/main.c
@@ -26,11 +26,40 @@ void	ft_usage(char *program_name)
 	printf("  %s[meals]%s      : Optional - number of times each philosopher must eat\n", BLUE, RESET);
 }
 
+void	*ft_philo(void *arg)
+{
+	int		id;
+
+	id = *(int *)arg;
+	printf("Philosopher #%d: düşünüyorum...\n", id);
+	usleep(500000);
+	printf("Philosopher #%d: öyleyse varım\n", id);
+	return (NULL);
+}
+
 int main(int ac, char **av)
 {
-    if (ac != 4 || ac != 5)
-    {
-        ft_usage(av[0]);
-        return (0);
-    }
+	pthread_t	threads[MAX_PHILOSOPHERS];
+	int		id[MAX_PHILOSOPHERS];
+	int i;
+
+    // if (ac != 4 || ac != 5)
+    // {
+    //     ft_usage(av[0]);
+    //     return (0);
+    // }
+
+	i = 0;
+	while (i < MAX_PHILOSOPHERS)
+	{
+		id[i] = i + 1;
+		pthread_create(&threads[i], NULL, ft_philo, &id[i]);
+		i++;
+	}
+	i = 0;
+	while (i < MAX_PHILOSOPHERS)
+	{
+		pthread_join(threads[i], NULL);
+		i++;
+	}
 }
